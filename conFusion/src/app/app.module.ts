5417ab58c22487c914e380e18b48b39382b419bd
YJ Tristan Kim <yjtkimsfu@gmail.com>
2019-04-25 05:41:48 -0700

    Week1 - Component Part 1

--- conFusion/src/app/app.module.ts
+++ conFusion/src/app/app.module.ts
@@ -3,18 +3,22 @@ import { NgModule } from '@angular/core';
 import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
 import { MatToolbarModule } from '@angular/material/toolbar';
 import { FlexLayoutModule } from '@angular/flex-layout';
-
-import { AppComponent } from './app.component';
+import { MatListModule } from '@angular/material/list';
 
 import 'hammerjs';
 
+import { AppComponent } from './app.component';
+import { MenuComponent } from './menu/menu.component';
+
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    MenuComponent
   ],
   imports: [
     BrowserModule,
     BrowserAnimationsModule,
+    MatListModule,
     FlexLayoutModule,
     MatToolbarModule
   ],
