f29a567f98d045dc96868ac44893265f0c3605ed
YJ Tristan Kim <yjtkimsfu@gmail.com>
2019-04-30 12:44:20 -0700

    Week 2 - SPA Part 2

--- conFusion/src/app/app-routing/routes.ts
+++ conFusion/src/app/app-routing/routes.ts
@@ -4,10 +4,12 @@ import { HomeComponent } from '../home/home.component';
 import { AboutComponent } from '../about/about.component';
 import { ContactComponent } from '../contact/contact.component';
 import { MenuComponent } from '../menu/menu.component';
+import { DishdetailComponent } from '../dishdetail/dishdetail.component';
 
 export const routes: Routes = [
   { path: 'home', component: HomeComponent},
   { path: 'menu', component: MenuComponent},
+  { path: 'dishdetail/:id', component: DishdetailComponent},
   { path: 'about', component: AboutComponent},
   { path: 'contactus', component: ContactComponent},
   { path: '', redirectTo: '/home', pathMatch: 'full'}
